64bb919e094fd64e721fdde0c10b787b0ed73ddc
1aimaneqbal <1aimaneqbal@gmail.com>
2021-06-11 23:29:10 +0530

    Final Commit

---
+++ app.js
@@ -0,0 +1,88 @@
+const CROSS_CLASS = 'cross'
+const CIRCLE_CLASS = 'circle'
+const WINNING_COMBINATIONS = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+]
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+const winningMessageElement = document.getElementById('winningMessage')
+const restartButton = document.getElementById('restartButton')
+const winningMessageTextElement = document.querySelector('[data-winning-message-text]')
+let circleTurn
+
+const startGame = () =>{
+  circleTurn = false
+  cellElements.forEach(cell => {
+    cell.classList.remove(CROSS_CLASS)
+    cell.classList.remove(CIRCLE_CLASS)
+    cell.removeEventListener('click', handleClick)
+    cell.addEventListener('click', handleClick, { once: true })
+  })
+  setBoardHoverClass()
+  winningMessageElement.classList.remove('show')
+}
+
+const handleClick = (e) =>{
+  const cell = e.target
+  const currentClass = circleTurn ? CIRCLE_CLASS : CROSS_CLASS
+  placeMark(cell, currentClass)
+  if (checkWin(currentClass)) {
+    endGame(false)
+  } else if (isDraw()) {
+    endGame(true)
+  } else {
+    swapTurns()
+    setBoardHoverClass()
+  }
+}
+
+const endGame = (draw) =>{
+  if (draw) {
+    winningMessageTextElement.innerText = 'Draw!'
+  } else {
+    winningMessageTextElement.innerText = `${circleTurn ? "Circle" : "Cross"} Wins!`
+  }
+  winningMessageElement.classList.add('show')
+}
+
+const isDraw = () =>{
+  return [...cellElements].every(cell => {
+    return cell.classList.contains(CROSS_CLASS) || cell.classList.contains(CIRCLE_CLASS)
+  })
+}
+
+const placeMark = () =>{
+  cell.classList.add(currentClass)
+}
+
+const swapTurns = () =>{
+  circleTurn = !circleTurn
+}
+
+const setBoardHoverClass = () =>{
+  board.classList.remove(CROSS_CLASS)
+  board.classList.remove(CIRCLE_CLASS)
+  if (circleTurn) {
+    board.classList.add(CIRCLE_CLASS)
+  } else {
+    board.classList.add(CROSS_CLASS)
+  }
+}
+
+const checkWin = () =>{
+  return WINNING_COMBINATIONS.some(combination => {
+    return combination.every(index => {
+      return cellElements[index].classList.contains(currentClass)
+    })
+  })
+}
+
+startGame()
+restartButton.addEventListener('click', startGame)
